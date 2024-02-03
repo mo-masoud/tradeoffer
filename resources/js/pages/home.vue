19675ce924da5f3afcff7bad52f65c98b55993c8
Mohamed Masoud <mo.masoud.7007@gmail.com>
2024-02-03 23:32:09 +0200

    feat: Install vue shadcn

---
+++ resources/js/pages/home.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import {Head} from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Home"/>
+    <div class="flex items-center justify-center h-screen bg-gray-100">
+        <div class="text-center">
+            <h1 class="text-6xl font-bold">Coming Soon</h1>
+            <p class="text-xl mt-4">Our website is under construction. We'll be here soon with our new awesome site.</p>
+        </div>
+    </div>
+</template>
