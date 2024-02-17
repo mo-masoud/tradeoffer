d81f361d4591e74df2bd33abf9f74aae693eb9ce
Mohamed Masoud <mo.masoud.7007@gmail.com>
2024-02-17 16:39:30 +0200

    hotfix: initial load permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Permission;
 use App\Models\User;
+use Exception;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,10 +26,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Permission::with('roles')->get()->each(function ($permission) {
-            Gate::define($permission->name, function (User $user) use ($permission) {
-                return $user->hasPermission($permission->name);
+        try {
+            Permission::with('roles')->get()->each(function ($permission) {
+                Gate::define($permission->name, function (User $user) use ($permission) {
+                    return $user->hasPermission($permission->name);
+                });
             });
-        });
+        } catch (Exception $e) {
+            // Do nothing
+        }
     }
 }
